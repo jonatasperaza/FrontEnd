c5aff685daab954be9a5557de6cafd930b87a8b7
PedroHenmalaquias <pedromalaquias2223@gmail.com>
2024-08-25 18:40:55 -0300

    FEAT: full web header #19

---
+++ src/components/header/NavComp.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+<template>
+    <nav>
+        <div class="nav-link"><RouterLink>Home</RouterLink></div>
+        <div class="nav-link"><RouterLink>Sobre nós</RouterLink></div>
+        <div class="nav-link"><RouterLink>Veículos</RouterLink></div>
+        <div class="nav-link"><RouterLink>Pedido</RouterLink></div>
+    </nav>
+</template>
+<style scoped lang="scss">
+@use '../../assets/main';
+nav{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+.nav-link{
+    color: main.$standard-white;
+    font-size: 20px;
+    font-weight: 500;
+    cursor: pointer;
+    a{
+        text-decoration: none;
+        color: main.$standard-white;
+    }
+}
+</style>
