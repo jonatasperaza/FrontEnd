1463d3275bb671b39e3696b24ca6bef55c0d1175
PedroHenmalaquias <pedromalaquias2223@gmail.com>
2024-08-31 10:55:25 -0300

    CHORE: first section done, second section in progress #29

---
+++ src/components/about/SecondSection.vue
@@ -0,0 +1,44 @@
+<script setup>
+</script>
+<template>
+    <section>
+        <span></span>
+        <div class="container">
+            <div class="cards"><div><img src="/public/handshake-icon.svg" alt=""><p></p></div></div>
+            <div class="textContent"></div>
+        </div>
+        <span></span>
+    </section>
+</template>
+<style scoped lang="scss">
+@use '../../assets/main';
+section {
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+background-color: main.$standard-black;
+.container{
+    width: 100%;
+    height: 25rem;
+    display: flex;
+    .cards{
+        width: 55%;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        div{
+            width: 10rem;
+            height: 10rem;
+            display: flex;
+            flex-direction: column;
+        }
+    }
+}
+span{
+    width: 100%;
+    height: 2px;
+    background-color: main.$standard-pink;
+}
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import FirstSection from "@/components/about/FirstSection.vue";
+import SecondSection from "@/components/about/SecondSection.vue";
+import HeaderComp from "@/components/header/HeaderComp.vue";
+</script>
+<template>
+    <HeaderComp />
+<FirstSection />
+<SecondSection />
+</template>
+<style scoped>
+</style>
