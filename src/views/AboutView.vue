56517b0dc96afc5672d65b3c701c2622bfc9211f
PedroHenmalaquias <pedromalaquias2223@gmail.com>
2024-09-01 10:54:04 -0300

    FEAT: fifth section done, all about view done #29

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,14 +3,14 @@ import FirstSection from "@/components/about/FirstSection.vue";
 import SecondSection from "@/components/about/SecondSection.vue";
 import ThirdSection from "@/components/about/ThirdSection.vue";
 import FourthSection from "@/components/about/FourthSection.vue";
-import HeaderComp from "@/components/header/HeaderComp.vue";
+import FifthSection from "@/components/about/FifthSection.vue";
 </script>
 <template>
-    <HeaderComp />
 <FirstSection />
 <SecondSection />
 <ThirdSection />
 <FourthSection />
+<FifthSection />
 </template>
 <style scoped>
 </style>
