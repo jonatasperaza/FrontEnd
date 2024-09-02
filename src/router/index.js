a42e9bd1e093e0c0bdb2c3dbbf2c24aa7392a767
jonatasperaza <jonatassilvaperaza@gmail.com>
2024-09-02 15:11:25 -0300

    Feat: create cep srvices, stores and views [Refs: #34]

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
+import ViaCepView from '@/views/tests/ViaCepView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       name: 'about',
       component: AboutView
     },
+    {
+      path: '/test',
+      name: 'test',
+      component: ViaCepView
+    }
   ]
 })
 
