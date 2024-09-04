bbe3e336ef5ce1b7d7c2e1a0e3cb89940339fc10
PedroHenmalaquias <pedromalaquias2223@gmail.com>
2024-09-04 10:14:51 -0300

    FEAT: register client view done [REFS: #36]

--- src/components/RegisterClient/FormSecondStep.vue
+++ src/components/RegisterClient/FormSecondStep.vue
@@ -1,75 +1,74 @@
-<script setup>
-</script>
+<script setup></script>
 <template>
-    <form @submit.prevent>
-        <label for="">Estado</label>
-        <input type="text" placeholder="Insira seu estado">
-        <label for="">Cidade</label>
-        <input type="text" placeholder="Insira sua cidade">
-        <label for="">CEP</label>
-        <input type="text" placeholder="Insira seu CEP">
-        <label for="">Bairro</label>
-        <input type="text" placeholder="Insira seu bairro">
-        <label for="">Endereço</label>
-        <input type="text" placeholder="Insira seu endereço">
-        <label for="">Número</label>
-        <input type="text" placeholder="Insira seu número">
-        <label for="">Complemento</label>
-        <input type="text" placeholder="Insira um complemento">
-        <button class="normalColor" @click="$emit('next')">Finalizar</button>
-        <button class="invertColor" @click="$emit('back')">Voltar</button>
-    </form>
+  <form @submit.prevent>
+    <label for="">Estado</label>
+    <input type="text" placeholder="Insira seu estado" />
+    <label for="">Cidade</label>
+    <input type="text" placeholder="Insira sua cidade" />
+    <label for="">CEP</label>
+    <input type="text" placeholder="Insira seu CEP" />
+    <label for="">Bairro</label>
+    <input type="text" placeholder="Insira seu bairro" />
+    <label for="">Endereço</label>
+    <input type="text" placeholder="Insira seu endereço" />
+    <label for="">Número</label>
+    <input type="text" placeholder="Insira seu número" />
+    <label for="">Complemento</label>
+    <input type="text" placeholder="Insira um complemento" />
+    <button class="normalColor" @click="$emit('next')">Finalizar</button>
+    <button class="invertColor" @click="$emit('back')">Voltar</button>
+  </form>
 </template>
 <style scoped lang="scss">
 @use '../../assets/main';
-form{
-        width: 50%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        margin: 1rem 0 4rem 0;
-        label{
-            color: main.$standard-white;
-            width: 50%;
-            font-size: 16px;
-            font-weight: 800;
-            margin: .5rem .5rem;
-        }
-        input{
-            height: 3.25rem;
-            border-radius: 1rem;
-            border: 1px solid main.$standard-white;
-            padding: 0 10px;
-            color: main.$standard-white;
-            background-color: main.$standard-black;
-        }
-        button{
-            width: 100%;
-            height: 3rem;
-            border: none;
-            border-radius: .5rem;
-            font-size: 16px;
-            font-weight: 500;
-            cursor: pointer;
-            margin: 1rem 0 0 0;
-            transition: .3s ease-in-out;
-        }
-    }
-.normalColor{
-    background-color: main.$standard-pink;
+form {
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 1rem auto 4rem auto;
+  label {
     color: main.$standard-white;
-}
-.invertColor{
-    background-color: main.$standard-black;
-    color: main.$standard-pink;
-    border: 2px solid main.$standard-pink;
-}
-.normalColor:hover{
-    background-color: main.$standard-black;
-    color: main.$standard-pink;
-}
-.invertColor:hover{
-    background-color: main.$standard-pink;
+    width: 50%;
+    font-size: 16px;
+    font-weight: 800;
+    margin: 0.5rem 0.5rem;
+  }
+  input {
+    height: 3.25rem;
+    border-radius: 1rem;
+    border: 1px solid main.$standard-white;
+    padding: 0 10px;
     color: main.$standard-white;
+    background-color: main.$standard-black;
+  }
+  button {
+    width: 100%;
+    height: 3rem;
+    border: none;
+    border-radius: 0.5rem;
+    font-size: 16px;
+    font-weight: 500;
+    cursor: pointer;
+    margin: 1rem 0 0 0;
+    transition: 0.3s ease-in-out;
+  }
+}
+.normalColor {
+  background-color: main.$standard-pink;
+  color: main.$standard-white;
+}
+.invertColor {
+  background-color: main.$standard-black;
+  color: main.$standard-pink;
+  border: 2px solid main.$standard-pink;
+}
+.normalColor:hover {
+  background-color: main.$standard-black;
+  color: main.$standard-pink;
+}
+.invertColor:hover {
+  background-color: main.$standard-pink;
+  color: main.$standard-white;
 }
 </style>
--- src/components/RegisterClient/RegisterComp.vue
+++ src/components/RegisterClient/RegisterComp.vue
@@ -1,133 +1,130 @@
 <script setup>
 import { ref } from 'vue'
-import FormFirstStep from './FormFirstStep.vue';
-import FormSecondStep from './FormSecondStep.vue';
+import FormFirstStep from './FormFirstStep.vue'
+import FormSecondStep from './FormSecondStep.vue'
 
-const firstStep = ref(null);
-const secondStep = ref(null);
-const isSecondStepVisible = ref(false);
-
+const firstStep = ref(null)
+const secondStep = ref(null)
+const isSecondStepVisible = ref(false)
 const next = () => {
-    // Remove any previous animation classes
-    firstStep.value.classList.remove('slideInReverse', 'slideOut');
-    secondStep.value.classList.remove('slideIn', 'slideOutReverse');
-
-    firstStep.value.classList.add('slideOut');
-    setTimeout(() => {
-        isSecondStepVisible.value = true;
-        firstStep.value.style.display = 'none';
-        secondStep.value.classList.add('slideIn');
-    }, 500);
-};
+  firstStep.value.classList.remove('slideInReverse', 'slideOut')
+  secondStep.value.classList.remove('slideIn', 'slideOutReverse')
+  firstStep.value.classList.add('slideOut')
+  setTimeout(() => {
+    isSecondStepVisible.value = true
+    firstStep.value.style.display = 'none'
+    secondStep.value.classList.add('slideIn')
+  }, 500)
+}
 const back = () => {
-    secondStep.value.classList.add('slideOutReverse');
+  secondStep.value.classList.add('slideOutReverse')
+  setTimeout(() => {
+    isSecondStepVisible.value = false
+    firstStep.value.style.display = 'flex'
+    secondStep.value.classList.remove('slideOutReverse')
     setTimeout(() => {
-        isSecondStepVisible.value = false;
-        firstStep.value.style.display = 'flex';
-        secondStep.value.classList.remove('slideOutReverse');
-        setTimeout(() =>{
-        firstStep.value.classList.add('slideInReverse');
-        }, 0)
-}, 500);
-};
+      firstStep.value.classList.add('slideInReverse')
+    }, 0)
+  }, 500)
+}
 </script>
 <template>
-<section>
+  <section>
     <div class="container">
-        <h1>Registre-se</h1>
-        <div ref="firstStep" :class="{ hidden: isSecondStepVisible }">
-            <FormFirstStep @next="next" />
-        </div>
-        <div ref="secondStep" v-show="isSecondStepVisible">
-            <FormSecondStep @back="back" />
-        </div>
+      <h1>Registre-se</h1>
+      <div ref="firstStep" :class="{ hidden: isSecondStepVisible }">
+        <FormFirstStep @next="next" />
+      </div>
+      <div ref="secondStep" v-show="isSecondStepVisible">
+        <FormSecondStep @back="back" />
+      </div>
     </div>
-</section>
+  </section>
 </template>
 
 <style scoped lang="scss">
 @use '../../assets/main';
-section{
-    width: 100%;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-image: url(/public/backgroundLogin.png);
-    background-size: cover;
+section {
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-image: url(/public/backgroundLogin.png);
+  background-size: cover;
 }
-div{
-    width: 100%;
+div {
+  width: 100%;
 }
-h1{
-    color: main.$standard-pink;
-    font-style: italic;
-    font-size: 50px;
-    margin: 2rem;
-    font-weight: 800;
+h1 {
+  color: main.$standard-pink;
+  font-style: italic;
+  font-size: 50px;
+  margin: 2rem;
+  font-weight: 800;
 }
-.container{
-    width: 45%;
-    min-height:100vh ;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: main.$standard-black;
+.container {
+  width: 45%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: main.$standard-black;
 }
 .hidden {
-    display: none;
+  display: none;
 }
 .slideOut {
-    animation: slideOut 0.5s forwards;
+  animation: slideOut 0.5s forwards;
 }
 .slideIn {
-    animation: slideIn 0.5s forwards;
+  animation: slideIn 0.5s forwards;
 }
 .slideInReverse {
-    animation: slideInReverse 0.5s forwards;
+  animation: slideInReverse 0.5s forwards;
 }
 .slideOutReverse {
-    animation: slideOutReverse 0.5s forwards;
+  animation: slideOutReverse 0.5s forwards;
 }
 @keyframes slideOut {
-    from {
-        opacity: 1;
-        transform: translateX(0);
-    }
-    to {
-        opacity: 0;
-        transform: translateX(-100%);
-    }
+  from {
+    opacity: 1;
+    transform: translateX(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateX(-100%);
+  }
 }
 @keyframes slideIn {
-    from {
-        opacity: 0;
-        transform: translateX(100%);
-    }
-    to {
-        opacity: 1;
-        transform: translateX(0);
-    }
+  from {
+    opacity: 0;
+    transform: translateX(100%);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
 }
 @keyframes slideOutReverse {
-    from {
-        opacity: 1;
-        transform: translateX(0);
-    }
-    to {
-        opacity: 0;
-        transform: translateX(100%);
-    }
+  from {
+    opacity: 1;
+    transform: translateX(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateX(100%);
+  }
 }
 @keyframes slideInReverse {
-    from {
-        opacity: 0;
-        transform: translateX(-100%);
-    }
-    to {
-        opacity: 1;
-        transform: translateX(0);
-    }
+  from {
+    opacity: 0;
+    transform: translateX(-100%);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
 }
 </style>
