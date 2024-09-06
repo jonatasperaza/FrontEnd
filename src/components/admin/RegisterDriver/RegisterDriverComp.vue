7e672694cca77c1155cff413670831d8882fded2
PedroHenmalaquias <pedromalaquias2223@gmail.com>
2024-09-06 16:42:59 -0300

    CHORE: register driver in progress [REFS: #44]

---
+++ src/components/admin/RegisterDriver/RegisterDriverComp.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+<template>
+    <section><div class="container"></div></section>
+</template>
+<style scoped lang="scss">
+@use '../../../assets/main';
+section{
+    width: 100%;
+    min-height: 100vh;
+    background-image: url(/public/backgroundLogin.png);
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+}
+.container {
+  width: 45%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: main.$standard-black;
+}
+</style>
