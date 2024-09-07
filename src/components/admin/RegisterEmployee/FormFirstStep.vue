35438a9a791f859261e1cff82d69a76597160ccb
rumians <ruanfort12@gmail.com>
2024-09-07 12:21:57 -0300

    CHORE: Employee registration in vue done [REFS: #45]

--- src/components/admin/RegisterEmployee/FormFirstStep.vue
+++ src/components/admin/RegisterEmployee/FormFirstStep.vue
@@ -1,6 +1,5 @@
 <script setup></script>
 <template>
-    <section>
         <form @submit.prevent>
             <label for="">Nome</label>
             <input type="text" placeholder="Insira seu nome" />
@@ -12,10 +11,100 @@
             <input type="text" placeholder="Insira seu CPF" />
             <label for="">Telefone</label>
             <input type="tel" placeholder="Insira seu telefone" />
+            <label for="">Cargo</label>
+            <select name="opcoes" id="opcoes"></select>
+            <div>
+                <label for="">É staff?</label>
+                <button>Sim</button>
+                <button>Não</button>
+            </div>
+            <div>
+                <label for="">Gênero</label>
+                <button><img src="../../../../public/imgs/Man.png" alt="Homem "></button>
+                <button><img src="../../../../public/imgs/Woman.png" alt="Mulher"></button>
+            </div>
             <button @click="$emit('next')">Próximo</button>
         </form>
-    </section>
 </template>
 <style scoped lang="scss">
 @use '../../../assets/main';
+form {
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin: 1rem auto 4rem auto;
+
+    div {
+        margin-top: 1em;
+        display: grid;
+        align-items: center;
+        justify-items: center;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: 2rem 1fr;
+        width: 100%;
+
+        label {
+            grid-column: 1 / 2 span;
+            width: 3.5em;
+        }
+
+        button {
+            grid-row: 2 ;
+            height: 2.5rem;
+            width: 90%;
+            border-radius: 1rem;
+            border: 1px solid main.$standard-white;
+            padding: 0 10px;
+            color: main.$standard-white;
+            background-color: main.$standard-black;
+        }
+
+    }
+
+    label {
+        color: main.$standard-white;
+        width: 50%;
+        height: 1.5em;
+        font-size: 16px;
+        font-weight: 800;
+        margin: 0.5rem 0.5rem;
+    }
+
+    input {
+        height: 3.25rem;
+        border-radius: 1rem;
+        border: 1px solid main.$standard-white;
+        padding: 0 10px;
+        color: main.$standard-white;
+        background-color: main.$standard-black;
+    }
+
+    select {
+        height: 2rem;
+        border-radius: 1em;
+        border: 1px solid main.$standard-white;
+        color: main.$standard-white;
+        background-color: main.$standard-black;
+    }
+
+    button {
+        width: 100%;
+        height: 3rem;
+        border: none;
+        border-radius: 0.5rem;
+        color: main.$standard-white;
+        background-color: main.$standard-pink;
+        font-size: 16px;
+        font-weight: 500;
+        cursor: pointer;
+        margin: 1rem 0;
+        transition: 0.3s ease-in-out;
+    }
+}
+
+button:hover {
+    background-color: main.$standard-black;
+    color: main.$standard-pink;
+}
 </style>
--- src/components/admin/RegisterEmployee/RegisterComp.vue
+++ src/components/admin/RegisterEmployee/RegisterComp.vue
@@ -1,20 +1,150 @@
 <script setup>
+import { ref } from 'vue'
 import FormFirstStep from './FormFirstStep.vue';
+import FormSecondStep from './FormSecondStep.vue';
+const firstStep = ref(null)
+const secondStep = ref(null)
+const isSecondStepVisible = ref(false)
+const next = () => {
+  firstStep.value.classList.remove('slideInReverse', 'slideOut')
+  secondStep.value.classList.remove('slideIn', 'slideOutReverse')
+  firstStep.value.classList.add('slideOut')
+  setTimeout(() => {
+    isSecondStepVisible.value = true
+    firstStep.value.style.display = 'none'
+    secondStep.value.classList.add('slideIn')
+  }, 500)}
+const back = () => {
+ secondStep.value.classList.add('slideOutReverse')
+  setTimeout(() => {
+    isSecondStepVisible.value = false
+    firstStep.value.style.display = ''
+    secondStep.value.classList.remove('slideOutReverse')
+    setTimeout(() => {
+      firstStep.value.classList.add('slideInReverse')
+    }, 0)
+  }, 500)}
 </script>
 <template>
-    <section>
+  <section>
     <div class="container">
-        <div><h1>Dados <span>Pessoais</span></h1></div>
-      <h1>Registre-se</h1>
-      <div>
-        <FormFirstStep/>
+      <div ref="firstStep" :class="{ hidden: isSecondStepVisible }">
+        <div class="container-title">
+          <h1>Dados <span class="white">Pessoais</span></h1>
+        </div>
+        <FormFirstStep @next="next" />
       </div>
-      <!-- <div ref="secondStep" v-show="isSecondStepVisible">
+      <div ref="secondStep" v-show="isSecondStepVisible">
+        <div class="container-title-Second-Steap">
+          <h1>Endereço</h1>
+        </div>
         <FormSecondStep @back="back" />
-      </div> -->
+      </div>
     </div>
   </section>
 </template>
 <style scoped lang="scss">
 @use '../../../assets/main';
+section {
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-image: url(/public/backgroundLogin.png);
+  background-size: cover;
+}
+div {
+  width: 100%;
+}
+.container-title {
+  display: flex;
+  justify-content: center;
+
+  h1 {
+    width: 9em;
+    border-left: 2px solid main.$standard-pink;
+
+  }
+}
+.container-title-Second-Steap {
+  display: flex;
+  justify-content: center;
+
+  h1 {
+    width: 5em;
+    border-left: 2px solid main.$standard-pink;
+  }
+}
+h1 {
+  color: main.$standard-pink;
+  font-style: italic;
+  font-size: 50px;
+  padding: 0 0 0 0.5em;
+  font-weight: 800;
+}
+.container {
+  width: 45%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: main.$standard-black;
+}
+.hidden {
+  display: none;
+}
+.slideOut {
+  animation: slideOut 0.5s forwards;
+}
+.slideIn {
+  animation: slideIn 0.5s forwards;
+}
+.slideInReverse {
+  animation: slideInReverse 0.5s forwards;
+}
+.slideOutReverse {
+  animation: slideOutReverse 0.5s forwards;
+}
+@keyframes slideOut {
+  from {
+    opacity: 1;
+    transform: translateX(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateX(-20%);
+  }
+}
+@keyframes slideIn {
+  from {
+    opacity: 0;
+    transform: translateX(20%);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+@keyframes slideOutReverse {
+  from {
+    opacity: 1;
+    transform: translateX(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateX(20%);
+  }
+}
+@keyframes slideInReverse {
+  from {
+    opacity: 0;
+    transform: translateX(-20%);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
 </style>
