a43ff4275fbdbe3d8b1109f17fb44e794a976493
jonatasperaza <jonatassilvaperaza@gmail.com>
2024-09-09 14:58:21 -0300

    CHORE: changing the user login [REFs: #43]

--- src/stores/auth/auth.js
+++ src/stores/auth/auth.js
@@ -1,89 +1,50 @@
-import { computed } from 'vue'
 import router from '@/router'
 import { defineStore } from 'pinia'
 import { useStorage } from '@vueuse/core'
 import { AuthService } from '@/services'
 
 export const useAuthStore = defineStore('auth', () => {
-  const localUser = {
+  const state = useStorage('user', {
+    user: {},
     isLogged: false,
-    email: '',
-    telephone: '',
-    token: '',
-    id: '',
-    name: ''
-  }
-
-  const userStorageLocal = useStorage('user', localUser)
+    error: false,
+    isLoading: false,
+    type: '',
+    token: ''
+  })
 
   async function setToken(token) {
     let data
     try {
+      state.value.isLoading = true
       data = await AuthService.getUser(token)
     } catch (error) {
       console.log(error)
+      error.value = true
     } finally {
-      userStorageLocal.value = {
-        isLogged: true,
-        email: data.email,
-        token: token,
-        id: data.id,
-        name: data.name
-      }
+      console.log(data)
+      state.value.user = data
+      state.value.isLogged = true
+      state.value.type = data.type
     }
   }
 
-  const isLogged = computed(() => userStorageLocal.value.isLogged)
-  const email = computed(() => userStorageLocal.value.email)
-  const token = computed(() => userStorageLocal.value.token)
-  const id = computed(() => userStorageLocal.value.id)
-  const name = computed(() => userStorageLocal.value.name)
-
-  function setIsLogged(novoDado) {
-    isLogged.value = novoDado
-  }
-
-  function setEmail(novoDado) {
-    email.value = novoDado
-  }
-
-  function setTokenPinia(novoDado) {
-    token.value = novoDado
-  }
-
-  function setId(novoDado) {
-    id.value = novoDado
-  }
-
-  function setName(novoDado) {
-    name.value = novoDado
-  }
-
   function unsetToken() {
-    userStorageLocal.value = {
+    state.value = {
+      user: {},
       isLogged: false,
-      email: null,
-      token: null,
-      id: null,
-      name: ''
+      error: false,
+      isLoading: false,
+      type: '',
+      token: ''
     }
     localStorage.clear()
     router.push('/singin')
   }
 
   return {
-    localUser,
     setToken,
     unsetToken,
-    isLogged,
-    email,
-    token,
-    id,
-    setIsLogged,
-    setEmail,
-    setId,
-    setTokenPinia,
-    name,
-    setName
+    state
   }
 })
