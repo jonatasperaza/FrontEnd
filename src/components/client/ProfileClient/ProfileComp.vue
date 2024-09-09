7971416e24aa2729467e21b9412c5569f6641997
PedroHenmalaquias <pedromalaquias2223@gmail.com>
2024-09-09 14:53:43 -0300

    CHORE: client profile screen in progress [REFS: #49]

---
+++ src/components/client/ProfileClient/ProfileComp.vue
@@ -0,0 +1,27 @@
+<script setup>
+</script>
+<template>
+    <section>
+        <div class="container">
+            
+        </div>
+    </section>
+</template>
+<style scoped lang="scss">
+@use '../../../assets/main';
+section{
+    width: 100%;
+    min-height: 100vh;
+    background-color: main.$standard-black;
+    .container{
+        width: 60%;
+        height: 100%;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: 1fr 1fr;
+        align-items: center;
+        border: 2px solid white;
+        margin: auto;
+    }
+}
+</style>
---
+++ src/views/client/ProfileClientView.vue
@@ -0,0 +1,6 @@
+<script setup>
+import ProfileComp from '@/components/client/ProfileClient/ProfileComp.vue';
+</script>
+<template>
+    <ProfileComp />
+</template>
