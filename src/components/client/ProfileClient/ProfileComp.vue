506c81e021baade00135463f8ed40cf39ca22cac
PedroHenmalaquias <pedromalaquias2223@gmail.com>
2024-09-10 21:01:48 -0300

    CHORE: right inputs in progress [REFS: #49]

--- src/components/client/ProfileClient/ProfileComp.vue
+++ src/components/client/ProfileClient/ProfileComp.vue
@@ -1,85 +1,72 @@
 <script setup>
-import LeftProfileInputsVue from "./LeftProfileInputs.vue";
+import LeftProfileInputsVue from './LeftProfileInputs.vue'
+import RightProfileInputsVue from './RightProfileInputs.vue'
 </script>
 <template>
-    <section>
-        <div class="container">
-            <h1><span class="strong-pink">Meu</span> Perfil</h1>
-            <div class="rowInputs">
-                <div class="left">
-                    <LeftProfileInputsVue />
-                </div>
-                <div class="right"><div class="ClientPic"></div>
-            <div class="inputs"></div></div>
-            </div>
-            <span class="pinkLine"></span>
-            <div class="orders">
-                <div class="selectTypeOrder"></div>
-                <div class="orderList"></div>
-            </div>
+  <section>
+    <div class="container">
+      <h1><span class="strong-pink">Meu</span> Perfil</h1>
+      <div class="rowInputs">
+        <div class="left">
+          <LeftProfileInputsVue />
         </div>
-    </section>
+        <div class="right">
+            <RightProfileInputsVue />
+        </div>
+      </div>
+      <span class="pinkLine"></span>
+      <div class="orders">
+        <div class="selectTypeOrder"></div>
+        <div class="orderList"></div>
+      </div>
+    </div>
+  </section>
 </template>
 <style scoped lang="scss">
 @use '../../../assets/main';
-section{
-    width: 100%;
-    min-height: 100vh;
-    background-color: main.$standard-black;
-    .container{
-        width: 60%;
-        height: 100%;
-        border: 2px solid white;
-        margin: auto;
+section {
+  width: 100%;
+  min-height: 100vh;
+  background-color: main.$standard-black;
+  .container {
+    width: 60%;
+    height: 100%;
+    border: 2px solid white;
+    margin: auto;
+  }
+  h1 {
+    color: white;
+    font-size: 2rem;
+    text-align: center;
+    padding: 1rem;
+    border-bottom: 2px solid white;
+  }
+  .rowInputs {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    .left {
+      width: 45%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: end;
+      align-items: center;
+      border-right: 2px solid white;
     }
-    h1{
-        color: white;
-        font-size: 2rem;
-        text-align: center;
-        padding: 1rem;
-        border-bottom: 2px solid white;
+    .right {
+      width: 45%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: end;
+      align-items: center;
     }
-    .rowInputs{
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 1rem;
-        .left{
-            width: 40%;
-            height: 100%;
-            display: flex;
-            flex-direction: column;
-            justify-content: space-between;
-            align-items: center;
-            border-right: 2px solid white;
-        }
-        .right{
-            width: 60%;
-            height: 100%;
-            display: flex;
-            flex-direction: column;
-            justify-content: space-between;
-            align-items: center;
-            .ClientPic{
-                width: 100px;
-                height: 100px;
-                background-color: white;
-                border-radius: 50%;
-            }
-            .inputs{
-                width: 100%;
-                display: flex;
-                flex-direction: column;
-                justify-content: space-between;
-                align-items: center;
-                padding: 1rem;
-                border: 2px solid white;
-            }
-        }
-    }
+  }
 }
-div{
-    border: 1px solid white;
-    margin: 1rem 0;
+div {
+  border: 1px solid white;
+  margin: 1rem 0;
 }
 </style>
---
+++ src/components/client/ProfileClient/RightProfileInputs.vue
@@ -0,0 +1,47 @@
+<script setup>
+</script>
+<template>
+    <div class="clientPic"></div>
+    <form @submit.prevent>
+        <label for="">Nome</label>
+        <input type="text" placeholder="Nome">
+        <label for="">Email</label>
+        <input type="text" placeholder="Email">
+        <label for="">Telefone</label>
+        <input type="text" placeholder="Telefone">
+        <label for="">Endereço</label>
+        <input type="text" placeholder="Endereço">
+    </form>
+</template>
+<style scoped lang="scss">
+@use '../../../assets/main';
+.clientPic {
+  width: 30%;
+  height: 7rem;
+  background-color: main.$standard-white;
+  border-radius: 50%;
+  margin: 3rem auto;
+}
+form {
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 1rem auto 4rem auto;
+  label {
+    color: main.$standard-white;
+    width: 50%;
+    font-size: 16px;
+    font-weight: 800;
+    margin: 0.5rem 0.5rem;
+  }
+  input {
+    height: 2.75rem;
+    border-radius: 1rem;
+    border: 1px solid main.$standard-white;
+    padding: 0 10px;
+    color: main.$standard-white;
+    background-color: main.$standard-black;
+  }
+}
+</style>
