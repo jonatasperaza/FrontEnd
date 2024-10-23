405b255ee3b94e13c7fcc168dab2233354979113
rumians <ruanfort12@gmail.com>
2024-10-23 20:36:25 -0300

    FEAT: Completed order status screen [REFS:#60]

--- src/components/orderstatus/OrderStatusTitle.vue
+++ src/components/orderstatus/OrderStatusTitle.vue
@@ -1,12 +1,30 @@
 <script setup>
-    
+
 </script>
 <template>
     <section>
-        
+        <div class="container">
+            <h1 class="white">
+                <span class="strong-pink">Status</span>
+                do Pedido
+            </h1>
+        </div>
     </section>
 </template>
 <style scoped lang="scss">
-@use '../../assets/main';
 
+section {
+    width: 100%;
+    height: 5rem;
+    grid-column: 1 / span 2;
+
+    .container {
+        display: flex;
+        align-items: center;
+        h1 {
+            margin-left: 0.5em;
+            font-size: 4em;
+        }
+    }
+}
 </style>
