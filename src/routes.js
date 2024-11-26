c382bcc022d98e458a9190ab34e7bad30ebdf15c
jonatasperaza <jonatassilvaperaza@gmail.com>
2024-11-25 21:52:32 -0300

    Feat: adicionar geração de sitemap e atualizar scripts de build

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+export const routes = [
+  { path: '/', name: 'Home' },
+  { path: '/vehicles', name: 'Veiculos' },
+  { path: '/about', name: 'Sobre' },
+  { path: '/login', name: 'Login' },
+  { path: '/client/signin', name: 'Login Do Cliente' },
+  { path: '/admin/employee/signup', name: 'Cadastro de Funcionário' },
+  { path: '/admin/driver/signin', name: 'Login do Motorista' },
+  { path: '/admin/dashboard', name: 'Dashboard Admin' },
+  { path: '/client/profile', name: 'Perfil do Cliente' },
+  { path: '/driver/profile', name: 'Perfil do Motorista' },
+  { path: '/make-order', name: 'Criar Pedido' },
+  { path: '/order-status', name: 'Status do Pedido' }
+]
